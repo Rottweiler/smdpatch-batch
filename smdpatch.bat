f52bd88614bd9f32858f1608eaa439a929dc01ff
Rottweiler <rottweiler.hackhound@aol.com>
2015-06-12 22:22:51 +0200

    First commit

---
+++ smdpatch.bat
@@ -0,0 +1,12 @@
+@setlocal enableextensions enabledelayedexpansion
+@echo off
+echo ________________________________________________________________________________
+echo                          smdpatch.bat by Rottweiler
+echo                     https://github.com/Rottweiler/smdpatch
+echo[
+echo --------------------------------------------------------------------------------
+FOR %%f IN (*.smd) DO (
+	echo Patching %%f
+	more +1 "%%f" > "smdpatch_%%f"
+)
+pause
